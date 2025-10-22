784d3b9e769a041bc22c0f34ac5e43f7e590a863
Caterina E <caterinaesposito702@gmail.com>
2025-10-22 12:56:44 +0200

    updated nav bar for php file

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!-- navbar.php -->
+<header>
+  <div class="header-inner">
+    <h1>🍽️ Celeste's Favorite Places</h1>
+    <nav>
+      <ul class="nav-links">
+        <li><a href="home.php">Home</a></li>
+        <li><a href="places.php">Places</a></li>
+        <li><a href="foods.php">Foods</a></li>
+        <li><a href="top_rated.php">Top Rated</a></li>
+        <li><a href="history.php">History</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+ 
